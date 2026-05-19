0290194fe3caba8a7fd962b4fe7430843722de38
Yusif-Aslan <262947@student.pwr.edu.pl>
2026-05-19 20:02:19 +0200

    Migrations

---
+++ BankInfinity.Api/Migrations/20260517185239_AddEmailVerificationFields.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankInfinity.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerificationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailVerificationCode",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerificationCodeExpiry",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCodeExpiry",
+                table: "Users");
+        }
+    }
+}
